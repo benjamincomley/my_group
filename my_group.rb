a2ae2ac351689732f0ad87ccd144f4a77060d69d
benji <benjamincomley@gmail.com>
2017-03-28 11:22:35 +0200

    Removed quotation marks from array in Line 1, added [] to hash values on Line 8

--- my_group.rb
+++ my_group.rb
@@ -1,10 +1,10 @@
-my_group = ["person_1", "person_2", "person_3"]
+my_group = [person_1, person_2, person_3]
 
 person_1 = {name: "Ben", gender: "male", age: 32}
 person_2 = {name: "Lina", gender: "female", age: 29}
 person_3 = {name: "Elwood", gender: "male", age: 2}
 
 my_group.each do |person|
-  puts "#{person:name} is a #{person:age} year old #{person:gender}."
+  puts "#{person[:name]} is a #{person[:age]} year old #{person[:gender]}."
 
 end
